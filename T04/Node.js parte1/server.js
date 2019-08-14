96f7a61ede4aad87c69282669f7d7990ed2a2406
Arthur Teixeira <stag006@benner.com.br>
2019-08-14 11:50:26 -0300

    comit da primeira e metade da segunda aula de node

---
+++ T04/Node.js parte1/server.js
@@ -0,0 +1,68 @@
+const app = require('./src/config/custom-express.js');
+
+app.listen(3000, function() {
+    console.log('Servidor rodando na porta 3000');
+
+});
+
+app.get('/', function(req, resp) {
+    resp.send(
+        `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Casa do Código </h1>
+                </body> 
+            </html>
+        `
+    );
+});
+
+app.get('/livros', function(req, resp) {
+    resp.send(
+        `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Listagem de livros </h1>
+                </body> 
+            </html>
+        `
+    );
+});
+// const http = require('http');
+
+// const servidor = http.createServer(function (req, resp) {
+
+//     let html = '';
+//     if (req.url == '/') {
+//         html = `
+//             <html>
+//                 <head>
+//                     <meta charset="utf-8">
+//                 </head>
+//                 <body>
+//                     <h1> Casa do codigo </h1>
+//                 </body> 
+//             </html>
+//         `;
+//     } else if (req.url == '/livros')
+//         html = `
+//         <html>
+//             <head>
+//                 <meta charset="utf-8">
+//             </head>
+//             <body>
+//                 <h1> Listagem de livros </h1>
+//             </body> 
+//         </html>
+//     `
+//     // Outros else if
+
+//     resp.end(html);
+// });
+// servidor.listen(3000);
